fcdf07f76397f2c39df51dacc4b9b9fa5f756c0f
nekrodarkmoon <nekrodarkmoon@gmail.com>
2022-03-20 23:36:19 -0700

    TODO - Ex 1 Completed

--- Assignment_4/src/main.cpp
+++ Assignment_4/src/main.cpp
@@ -24,21 +24,21 @@ using namespace Eigen;
 class AABBTree
 {
 public:
-    class Node
-    {
-    public:
-        AlignedBox3d bbox;
-        int parent;   // Index of the parent node (-1 for root)
-        int left;     // Index of the left child (-1 for a leaf)
-        int right;    // Index of the right child (-1 for a leaf)
-        int triangle; // Index of the node triangle (-1 for internal nodes)
-    };
+  class Node
+  {
+  public:
+    AlignedBox3d bbox;
+    int parent;   // Index of the parent node (-1 for root)
+    int left;     // Index of the left child (-1 for a leaf)
+    int right;    // Index of the right child (-1 for a leaf)
+    int triangle; // Index of the node triangle (-1 for internal nodes)
+  };
 
-    std::vector<Node> nodes;
-    int root;
+  std::vector<Node> nodes;
+  int root;
 
-    AABBTree() = default;                           // Default empty constructor
-    AABBTree(const MatrixXd &V, const MatrixXi &F); // Build a BVH from an existing mesh
+  AABBTree() = default;                           // Default empty constructor
+  AABBTree(const MatrixXd &V, const MatrixXi &F); // Build a BVH from an existing mesh
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -79,49 +79,49 @@ const Vector4d ambient_light(0.2, 0.2, 0.2, 0);
 // Fills the different arrays
 void setup_scene()
 {
-    // Loads file
-    std::ifstream in(mesh_filename);
-    std::string token;
-    in >> token;
-    int nv, nf, ne;
-    in >> nv >> nf >> ne;
-    vertices.resize(nv, 3);
-    facets.resize(nf, 3);
-    for (int i = 0; i < nv; ++i)
-    {
-        in >> vertices(i, 0) >> vertices(i, 1) >> vertices(i, 2);
-    }
-    for (int i = 0; i < nf; ++i)
-    {
-        int s;
-        in >> s >> facets(i, 0) >> facets(i, 1) >> facets(i, 2);
-        assert(s == 3);
-    }
+  // Loads file
+  std::ifstream in(mesh_filename);
+  std::string token;
+  in >> token;
+  int nv, nf, ne;
+  in >> nv >> nf >> ne;
+  vertices.resize(nv, 3);
+  facets.resize(nf, 3);
+  for (int i = 0; i < nv; ++i)
+  {
+    in >> vertices(i, 0) >> vertices(i, 1) >> vertices(i, 2);
+  }
+  for (int i = 0; i < nf; ++i)
+  {
+    int s;
+    in >> s >> facets(i, 0) >> facets(i, 1) >> facets(i, 2);
+    assert(s == 3);
+  }
 
-    // setup tree
-    bvh = AABBTree(vertices, facets);
+  // setup tree
+  bvh = AABBTree(vertices, facets);
 
-    // Lights
-    light_positions.emplace_back(8, 8, 0);
-    light_colors.emplace_back(16, 16, 16, 0);
+  // Lights
+  light_positions.emplace_back(8, 8, 0);
+  light_colors.emplace_back(16, 16, 16, 0);
 
-    light_positions.emplace_back(6, -8, 0);
-    light_colors.emplace_back(16, 16, 16, 0);
+  light_positions.emplace_back(6, -8, 0);
+  light_colors.emplace_back(16, 16, 16, 0);
 
-    light_positions.emplace_back(4, 8, 0);
-    light_colors.emplace_back(16, 16, 16, 0);
+  light_positions.emplace_back(4, 8, 0);
+  light_colors.emplace_back(16, 16, 16, 0);
 
-    light_positions.emplace_back(2, -8, 0);
-    light_colors.emplace_back(16, 16, 16, 0);
+  light_positions.emplace_back(2, -8, 0);
+  light_colors.emplace_back(16, 16, 16, 0);
 
-    light_positions.emplace_back(0, 8, 0);
-    light_colors.emplace_back(16, 16, 16, 0);
+  light_positions.emplace_back(0, 8, 0);
+  light_colors.emplace_back(16, 16, 16, 0);
 
-    light_positions.emplace_back(-2, -8, 0);
-    light_colors.emplace_back(16, 16, 16, 0);
+  light_positions.emplace_back(-2, -8, 0);
+  light_colors.emplace_back(16, 16, 16, 0);
 
-    light_positions.emplace_back(-4, 8, 0);
-    light_colors.emplace_back(16, 16, 16, 0);
+  light_positions.emplace_back(-4, 8, 0);
+  light_colors.emplace_back(16, 16, 16, 0);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -130,31 +130,31 @@ void setup_scene()
 
 AlignedBox3d bbox_from_triangle(const Vector3d &a, const Vector3d &b, const Vector3d &c)
 {
-    AlignedBox3d box;
-    box.extend(a);
-    box.extend(b);
-    box.extend(c);
-    return box;
+  AlignedBox3d box;
+  box.extend(a);
+  box.extend(b);
+  box.extend(c);
+  return box;
 }
 
 AABBTree::AABBTree(const MatrixXd &V, const MatrixXi &F)
 {
-    // Compute the centroids of all the triangles in the input mesh
-    MatrixXd centroids(F.rows(), V.cols());
-    centroids.setZero();
-    for (int i = 0; i < F.rows(); ++i)
+  // Compute the centroids of all the triangles in the input mesh
+  MatrixXd centroids(F.rows(), V.cols());
+  centroids.setZero();
+  for (int i = 0; i < F.rows(); ++i)
+  {
+    for (int k = 0; k < F.cols(); ++k)
     {
-        for (int k = 0; k < F.cols(); ++k)
-        {
-            centroids.row(i) += V.row(F(i, k));
-        }
-        centroids.row(i) /= F.cols();
+      centroids.row(i) += V.row(F(i, k));
     }
+    centroids.row(i) /= F.cols();
+  }
 
-    // TODO:
+  // TODO:
 
-    // Split each set of primitives into 2 sets of roughly equal size,
-    // based on sorting the centroids along one direction or another.
+  // Split each set of primitives into 2 sets of roughly equal size,
+  // based on sorting the centroids along one direction or another.
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -165,36 +165,82 @@ double ray_triangle_intersection(const Vector3d &ray_origin, const Vector3d &ray
                                  const Vector3d &a, const Vector3d &b, const Vector3d &c,
                                  Vector3d &p, Vector3d &N)
 {
-    // TODO:
-    // Compute whether the ray intersects the given triangle.
-    // If you have done the parallelogram case, this should be very similar to it.
+  // TODO:
+  // Compute whether the ray intersects the given triangle.
+  // If you have done the parallelogram case, this should be very similar to it.
 
+  // Create Vectors
+  const Vector3d triag_origin = a;
+  const Vector3d triag_u = b - triag_origin;
+  const Vector3d triag_v = c - triag_origin;
+
+  // Set the correct intersection point, update p and N to the correct values
+  const Vector3d P = ray_direction.cross(triag_v);
+  const double det = triag_u.dot(P);
+  const double det_inverse = 1. / det;
+
+  const Vector3d T = ray_origin - triag_origin;
+  const Vector3d Q = T.cross(triag_u);
+
+  const double v1 = T.dot(P) * det_inverse;
+  const double v2 = ray_direction.dot(Q) * det_inverse;
+
+  if (v1 < 0 || v1 > 1 || v2 < 0 || v1 + v2 >= 1)
     return -1;
+
+  const double t = det_inverse * triag_v.dot(Q);
+  const Vector3d intersection = ray_origin + (ray_direction * t);
+
+  p = intersection;
+  N = triag_v.cross(triag_u).normalized();
+
+  return t;
 }
 
 bool ray_box_intersection(const Vector3d &ray_origin, const Vector3d &ray_direction, const AlignedBox3d &box)
 {
-    // TODO:
-    // Compute whether the ray intersects the given box.
-    // we are not testing with the real surface here anyway.
-    return false;
+  // TODO:
+  // Compute whether the ray intersects the given box.
+  // we are not testing with the real surface here anyway.
+  return false;
 }
 
 // Finds the closest intersecting object returns its index
 // In case of intersection it writes into p and N (intersection point and normals)
 bool find_nearest_object(const Vector3d &ray_origin, const Vector3d &ray_direction, Vector3d &p, Vector3d &N)
 {
-    int closest_index = -1;
-    double closest_t = std::numeric_limits<double>::max();
-    Vector3d tmp_p, tmp_N;
+  int closest_index = -1;
+  double closest_t = std::numeric_limits<double>::max();
+  Vector3d tmp_p, tmp_N;
 
-    // TODO:
-    // Method (1): Traverse every triangle and return the closest hit.
-    // Method (2): Traverse the BVH tree and test the intersection with a
-    // triangles at the leaf nodes that intersects the input ray.
-    // for (int i =0; i < )
+  // TODO:
+  // Method (1): Traverse every triangle and return the closest hit.
+  // Method (2): Traverse the BVH tree and test the intersection with a
+  // triangles at the leaf nodes that intersects the input ray.
+  for (int i = 0; i < facets.rows(); ++i)
+  {
+    const Vector3d a = vertices.row(facets.coeff(i, 0));
+    const Vector3d b = vertices.row(facets.coeff(i, 1));
+    const Vector3d c = vertices.row(facets.coeff(i, 2));
 
+    const double t = ray_triangle_intersection(ray_origin, ray_direction, a, b, c, tmp_p, tmp_N);
+
+    if (t >= 0)
+    {
+      if (t < closest_t)
+      {
+        closest_index = i;
+        closest_t = t;
+        p = tmp_p;
+        N = tmp_N;
+      }
+    }
+  }
+
+  if (closest_index < 0)
     return false;
+
+  return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -203,118 +249,118 @@ bool find_nearest_object(const Vector3d &ray_origin, const Vector3d &ray_directi
 
 Vector4d shoot_ray(const Vector3d &ray_origin, const Vector3d &ray_direction)
 {
-    // Intersection point and normal, these are output of find_nearest_object
-    Vector3d p, N;
+  // Intersection point and normal, these are output of find_nearest_object
+  Vector3d p, N;
 
-    const bool nearest_object = find_nearest_object(ray_origin, ray_direction, p, N);
+  const bool nearest_object = find_nearest_object(ray_origin, ray_direction, p, N);
 
-    if (!nearest_object)
-    {
-        // Return a transparent color
-        return Vector4d(0, 0, 0, 0);
-    }
+  if (!nearest_object)
+  {
+    // Return a transparent color
+    return Vector4d(0, 0, 0, 0);
+  }
 
-    // Ambient light contribution
-    const Vector4d ambient_color = obj_ambient_color.array() * ambient_light.array();
+  // Ambient light contribution
+  const Vector4d ambient_color = obj_ambient_color.array() * ambient_light.array();
 
-    // Punctual lights contribution (direct lighting)
-    Vector4d lights_color(0, 0, 0, 0);
-    for (int i = 0; i < light_positions.size(); ++i)
-    {
-        const Vector3d &light_position = light_positions[i];
-        const Vector4d &light_color = light_colors[i];
+  // Punctual lights contribution (direct lighting)
+  Vector4d lights_color(0, 0, 0, 0);
+  for (int i = 0; i < light_positions.size(); ++i)
+  {
+    const Vector3d &light_position = light_positions[i];
+    const Vector4d &light_color = light_colors[i];
 
-        Vector4d diff_color = obj_diffuse_color;
+    Vector4d diff_color = obj_diffuse_color;
 
-        // Diffuse contribution
-        const Vector3d Li = (light_position - p).normalized();
-        const Vector4d diffuse = diff_color * std::max(Li.dot(N), 0.0);
+    // Diffuse contribution
+    const Vector3d Li = (light_position - p).normalized();
+    const Vector4d diffuse = diff_color * std::max(Li.dot(N), 0.0);
 
-        // Specular contribution
-        const Vector3d Hi = (Li - ray_direction).normalized();
-        const Vector4d specular = obj_specular_color * std::pow(std::max(N.dot(Hi), 0.0), obj_specular_exponent);
-        // Vector3d specular(0, 0, 0);
+    // Specular contribution
+    const Vector3d Hi = (Li - ray_direction).normalized();
+    const Vector4d specular = obj_specular_color * std::pow(std::max(N.dot(Hi), 0.0), obj_specular_exponent);
+    // Vector3d specular(0, 0, 0);
 
-        // Attenuate lights according to the squared distance to the lights
-        const Vector3d D = light_position - p;
-        lights_color += (diffuse + specular).cwiseProduct(light_color) / D.squaredNorm();
-    }
+    // Attenuate lights according to the squared distance to the lights
+    const Vector3d D = light_position - p;
+    lights_color += (diffuse + specular).cwiseProduct(light_color) / D.squaredNorm();
+  }
 
-    // Rendering equation
-    Vector4d C = ambient_color + lights_color;
+  // Rendering equation
+  Vector4d C = ambient_color + lights_color;
 
-    // Set alpha to 1
-    C(3) = 1;
+  // Set alpha to 1
+  C(3) = 1;
 
-    return C;
+  return C;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void raytrace_scene()
 {
-    std::cout << "Simple ray tracer." << std::endl;
+  std::cout << "Simple ray tracer." << std::endl;
 
-    int w = 640;
-    int h = 480;
-    MatrixXd R = MatrixXd::Zero(w, h);
-    MatrixXd G = MatrixXd::Zero(w, h);
-    MatrixXd B = MatrixXd::Zero(w, h);
-    MatrixXd A = MatrixXd::Zero(w, h); // Store the alpha mask
+  int w = 640;
+  int h = 480;
+  MatrixXd R = MatrixXd::Zero(w, h);
+  MatrixXd G = MatrixXd::Zero(w, h);
+  MatrixXd B = MatrixXd::Zero(w, h);
+  MatrixXd A = MatrixXd::Zero(w, h); // Store the alpha mask
 
-    // The camera always points in the direction -z
-    // The sensor grid is at a distance 'focal_length' from the camera center,
-    // and covers an viewing angle given by 'field_of_view'.
-    double aspect_ratio = double(w) / double(h);
-    double image_y = focal_length * tan(field_of_view / 2);
-    double image_x = image_y * aspect_ratio;
+  // The camera always points in the direction -z
+  // The sensor grid is at a distance 'focal_length' from the camera center,
+  // and covers an viewing angle given by 'field_of_view'.
+  double aspect_ratio = double(w) / double(h);
+  double image_y = focal_length * tan(field_of_view / 2);
+  double image_x = image_y * aspect_ratio;
 
-    // The pixel grid through which we shoot rays is at a distance 'focal_length'
-    const Vector3d image_origin(-image_x, image_y, camera_position[2] - focal_length);
-    const Vector3d x_displacement(2.0 / w * image_x, 0, 0);
-    const Vector3d y_displacement(0, -2.0 / h * image_y, 0);
+  // The pixel grid through which we shoot rays is at a distance 'focal_length'
+  const Vector3d image_origin(-image_x, image_y, camera_position[2] - focal_length);
+  const Vector3d x_displacement(2.0 / w * image_x, 0, 0);
+  const Vector3d y_displacement(0, -2.0 / h * image_y, 0);
 
-    for (unsigned i = 0; i < w; ++i)
+  for (unsigned i = 0; i < w; ++i)
+  {
+    for (unsigned j = 0; j < h; ++j)
     {
-        for (unsigned j = 0; j < h; ++j)
-        {
-            const Vector3d pixel_center = image_origin + (i + 0.5) * x_displacement + (j + 0.5) * y_displacement;
+      const Vector3d pixel_center = image_origin + (i + 0.5) * x_displacement + (j + 0.5) * y_displacement;
 
-            // Prepare the ray
-            Vector3d ray_origin;
-            Vector3d ray_direction;
+      // Prepare the ray
+      Vector3d ray_origin;
+      Vector3d ray_direction;
 
-            if (is_perspective)
-            {
-                // Perspective camera
-                ray_origin = camera_position;
-                ray_direction = (pixel_center - camera_position).normalized();
-            }
-            else
-            {
-                // Orthographic camera
-                ray_origin = pixel_center;
-                ray_direction = Vector3d(0, 0, -1);
-            }
+      if (is_perspective)
+      {
+        // Perspective camera
+        ray_origin = camera_position;
+        ray_direction = (pixel_center - camera_position).normalized();
+      }
+      else
+      {
+        // Orthographic camera
+        ray_origin = pixel_center;
+        ray_direction = Vector3d(0, 0, -1);
+      }
 
-            const Vector4d C = shoot_ray(ray_origin, ray_direction);
-            R(i, j) = C(0);
-            G(i, j) = C(1);
-            B(i, j) = C(2);
-            A(i, j) = C(3);
-        }
+      const Vector4d C = shoot_ray(ray_origin, ray_direction);
+      R(i, j) = C(0);
+      G(i, j) = C(1);
+      B(i, j) = C(2);
+      A(i, j) = C(3);
     }
+  }
 
-    // Save to png
-    write_matrix_to_png(R, G, B, A, filename);
+  // Save to png
+  write_matrix_to_png(R, G, B, A, filename);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
-    setup_scene();
+  setup_scene();
 
-    raytrace_scene();
-    return 0;
+  raytrace_scene();
+  return 0;
 }
